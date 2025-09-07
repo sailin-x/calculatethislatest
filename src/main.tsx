def021412936fcf36723aa1ac5c3618b8277b03e
sailin <sailin.on@me.com>
2025-09-07 14:15:06 +1200

    Remove debug code that was disabling validation

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,3 @@
-// RADICAL SOLUTION - Test if site works without validation
-console.log('RADICAL SOLUTION: Testing site without complex validation - timestamp:', Date.now());
-
-// Simple global allInputs fallback
-if (typeof window !== 'undefined') {
-  (window as any).allInputs = {};
-}
-
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
